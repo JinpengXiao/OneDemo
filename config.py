8732690187d8d41a9cdf6f34f33fad4388becdbb
JinpengXiao <xjpxiao@cn.ibm.com>
2018-07-11 10:38:37 +0800

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ Created on Jun 10, 2017
 bluemix_watsonvoice = 'en-GB_KateVoice'
 @author: xiao jinpeng
 '''
-bluemix_username = "cb3f2b70-ef28-4d5c-a701-dfcc1b221a7b"
-bluemix_password = "DplVvSphoemC"
+bluemix_username = ""
+bluemix_password = ""
 
 bluemix_watsonvoice = 'en-US_AllisonVoice'
 
